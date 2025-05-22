764a699d51f5ddd65b2fc14a09d46feaf0f85087
ajith4Tech <ajith@tech4goodcommunity.com>
2025-05-22 13:35:00 +0530

    Fix: Updated institute profile and permissions

---
+++ sunbird/sunbird/doctype/sunbird_program_institute/sunbird_program_institute.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, T4GC and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sunbird program-institute", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ sunbird/sunbird/doctype/sunbird_program_institute/test_sunbird_program_institute.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, T4GC and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSunbirdprograminstitute(FrappeTestCase):
+	pass
